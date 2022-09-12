9b2d7c8ee239a1d4ec01eeb23d8bb5d2623d69fd
LeonardoRenoldi <95914933+LeonardoRenoldi@users.noreply.github.com>
2022-09-12 12:36:48 -0300

    Add files via upload

---
+++ Projt/botao.js
@@ -0,0 +1,62 @@
+const botaoCadastro = document.getElementById("botaoCadastro")
+const botaoLoja = document.getElementById("botaoLoja")
+
+function abrirCadastro() {
+    window.open("https://razor.com.br/minha-conta/")
+}
+
+//botaoCadastro.onclick = function(){     //botão login/cadastro
+//    window.open("https://razor.com.br/minha-conta/")
+//}
+
+function abrirProdutos() {   //botão loja geral
+    window.open("https://razor.com.br/produtos/%22");
+}
+
+function solicitarAjuda() {
+    window.open("https://razor.com.br/suporte/")
+}
+
+//botaoLoja.onclick = function(){   //botão loja geral
+//    window.open("https://razor.com.br/produtos/")
+//}
+
+botaoQuemSomosNos.onclick = function(){  //gustavo
+
+}
+
+botaoProdutoDestaque.onclick = function(){
+    window.open("https://razor.com.br/produto/prodigy-xp-960g35/")
+}
+
+botaoOrcamentos.onclick = function(){
+    window.open("https://workstation.razor.com.br")
+}
+
+botaoLINThomas.onclick = function(){
+    window.open("https://www.linkedin.com/in/thomasknack/")
+}
+
+botaoLINGustavo.onclick = function(){
+    window.open("https://www.linkedin.com/in/gustavo-bonemann-100317238/")
+}
+
+botaoLINGesiane.onclick = function(){
+    window.open("https://www.linkedin.com/in/gesiane-vingat-amorim-619534123/")
+}
+
+botaoLINWesley.onclick = function(){
+    window.open("https://www.linkedin.com/in/wesley-henrique-silva-da-silva-71bb5224b/")
+}
+
+botaoLINLeonardo.onclick = function(){
+    window.open("https://www.linkedin.com/in/leonardo-renoldi-7a5998120/")
+}
+
+
+
+//botao para https://razor.com.br/minha-conta/
+//botao para https://razor.com.br/produtos/
+//lista dup. encadeada ou carrossel para seção "escolha sua máquina"
+//botao para "quem somos nós"
+//list dup/carrossel para rodapé(participantes)
